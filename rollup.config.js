d972414248e0d999841af1e544a95881ca19b962
Georgios Giatsidis <georgiosgiatsidis@hotmail.gr>
2023-03-06 17:18:15 +0200

    Update all deps including rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import pkg from './package.json';
 
 export default {
@@ -19,13 +18,18 @@ export default {
         },
     ],
     plugins: [
-        resolve({
+        nodeResolve({
             mainFields: ['module'],
             extensions: ['.js'],
         }),
         commonjs(),
         babel({
+            babelHelpers: 'bundled',
             exclude: 'node_modules/**',
         }),
     ],
+    onwarn(warning, warn) {
+        if (warning.code === 'THIS_IS_UNDEFINED') return;
+        warn(warning);
+    },
 };
