d3b0c571f2be56a41b6fc8abb819e2e1eb074d72
Georgios Giatsidis <georgiosgiatsidis@hotmail.gr>
2023-03-08 11:18:53 +0200

    Include es module in npm package when published

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import pkg from './package.json';
+import { readFileSync } from 'fs';
+
+const pkg = JSON.parse(readFileSync('package.json', { encoding: 'utf8' }));
 
 export default {
     input: './index.js',
